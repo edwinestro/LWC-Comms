e3617b4baa3f057f58fb66c31eb8c5d155b2bf97
Edwin Estrada <edwin.estro@me.com>
2023-03-06 20:26:09 -0600

    lwcs childing

---
+++ Lwc comms/force-app/main/default/lwc/button/button.js
@@ -0,0 +1,15 @@
+import { LightningElement, api } from 'lwc';
+
+
+export default class Button extends LightningElement {
+
+    @api label;
+    @api icon;
+    handleButton(event) {
+      this.dispatchEvent(new CustomEvent('buttonclick',{
+         bubbles: true
+      }));
+    }
+
+
+}
